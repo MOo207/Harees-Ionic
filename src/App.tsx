0a77e555fbf5669446fe3c1c7ae9e85846fe88f2
MOo207 <mohammedismaeal522@gmail.com>
2022-02-16 12:56:03 +0300

    clean unused files

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
-import Result from './pages/Result';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -43,9 +42,6 @@ const App: React.FC = () => (
             </Route>
             <Route exact path="/home" >
               <Home />
-            </Route>
-            <Route exact path="/Result">
-              <Result />
             </Route>
           </IonRouterOutlet>
         </IonReactRouter>
